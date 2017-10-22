dc2ed551f3ff2c1d66e7dc490fd0cb673c92a5b0
spezi77 <spezi77@gmx.com>
2017-10-22 16:29:54 +0200

    tasks: bacon: Show meta information of the zip upon successful build. Squashed in: -Cut off the path information -Print package size in bytes instead of M. -Print build date as timestamp (Adjusted to new build system in Android 10)
    Change-Id: Id85663e6479d8368d95da0df95a693a547276adc
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -25,3 +25,10 @@ bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(PD_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(PD_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(PD_TARGET_PACKAGE).md5sum
 	$(hide) ./vendor/pixeldust/tools/pd.sh
+	@echo ""
+	@echo -e ${CL_CYN}"════════════════════════════════════════════════════════════════════════════════"${CL_RST}
+	@echo -e ${CL_CYN}"Package zip: "${CL_MAG} $(PIXELDUST_VERSION).zip                                 ${CL_RST}
+	@echo -e ${CL_CYN}"Package md5: "${CL_MAG}" `cat $(PD_TARGET_PACKAGE).md5sum | cut -d ' ' -f 1`   "${CL_RST}
+	@echo -e ${CL_CYN}"Package size:"${CL_MAG}" `ls -l $(PD_TARGET_PACKAGE) | cut -d ' ' -f 5`        "${CL_RST}
+	@echo -e ${CL_CYN}"Timestamp:   "${CL_MAG} $(BUILD_TIMESTAMP)                                        ${CL_RST}
+	@echo -e ${CL_CYN}"════════════════════════════════════════════════════════════════════════════════"${CL_RST}
