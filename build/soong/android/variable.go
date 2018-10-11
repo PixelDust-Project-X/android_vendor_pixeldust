f302cbc18dd77855fc14ef4f18ead9a435992a3d
Alessandro Astone <ales.astone@gmail.com>
2018-10-12 00:16:27 +0200

    soong_config: Add BOOTLOADER_MESSAGE_OFFSET
    * libbootloader_message now uses Android.bp
    
    Change-Id: I750a04be97dbe01564d45cd605744b15f209e408
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,8 +1,14 @@
 package android
 type Product_variables struct {
+
+	Bootloader_message_offset struct {
+		Cflags []string
+	}
+
 	Target_shim_libs struct {
 		Cppflags []string
 	}
+
 	Target_uses_color_metadata struct {
 		Cppflags []string
 	}
@@ -11,4 +17,5 @@ type Product_variables struct {
 type ProductVariables struct {
 	Target_shim_libs  *string `json:",omitempty"`
 	Target_uses_color_metadata  *bool `json:",omitempty"`
+	Bootloader_message_offset  *int `json:",omitempty"`
 }
