45adf43d096e6cea1901e79a69b512791df88f5b
spezi77 <spezi7713@gmx.net>
2019-09-22 11:42:07 +0200

    Revert "soong_config: Add BOOTLOADER_MESSAGE_OFFSET"
    This reverts commit f302cbc18dd77855fc14ef4f18ead9a435992a3d.

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,14 +1,8 @@
 package android
 type Product_variables struct {
-
-	Bootloader_message_offset struct {
-		Cflags []string
-	}
-
 	Target_shim_libs struct {
 		Cppflags []string
 	}
-
 	Target_uses_color_metadata struct {
 		Cppflags []string
 	}
@@ -17,5 +11,4 @@ type Product_variables struct {
 type ProductVariables struct {
 	Target_shim_libs  *string `json:",omitempty"`
 	Target_uses_color_metadata  *bool `json:",omitempty"`
-	Bootloader_message_offset  *int `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,7 +1,6 @@
 $(call add_json_map, Pixeldust)
-add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)))
 
 $(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Target_uses_color_metadata,            $(filter true,$(TARGET_USES_COLOR_METADATA)))
-$(call add_json_val_default, Bootloader_message_offset,      $(BOOTLOADER_MESSAGE_OFFSET), 0)
+
 $(call end_json_map)
