b5c56950cdd2d1063eeff0366717f2f5be39971d
spezi77 <spezi7713@gmx.net>
2019-09-25 18:47:50 +0200

    products: marlin: Check if the AOSP device configuration exists. This fixes a rare bug where the AOSP device repo has not yet been fetched due to a lack of a dependency sync.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- products/marlin/pixeldust_marlin.mk
+++ products/marlin/pixeldust_marlin.mk
@@ -29,7 +29,7 @@ TARGET_USES_AOSP := true
 TARGET_EXCLUDE_QCOM_VENDOR_SEPOLICY := true
 
 # Inherit AOSP device configuration for marlin.
-$(call inherit-product, device/google/marlin/aosp_marlin.mk)
+$(call inherit-product-if-exists, device/google/marlin/aosp_marlin.mk)
 
 # Include common PixelDust stuff
 include vendor/pixeldust/configs/pixeldust_phone.mk
