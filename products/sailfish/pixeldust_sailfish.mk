debd9312d17d751d3cef1cf71085b51aa5315a3d
spezi77 <spezi7713@gmx.net>
2019-10-07 23:55:33 +0200

    products: marlin/sailfish: Update fp desc for QP1A.191005.007.A1
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- products/sailfish/pixeldust_sailfish.mk
+++ products/sailfish/pixeldust_sailfish.mk
@@ -52,9 +52,9 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sailfish \
-    PRIVATE_BUILD_DESC="sailfish-user 10 QP1A.190711.020 5800535 release-keys"
+    PRIVATE_BUILD_DESC="sailfish-user 10 QP1A.191005.007.A1 5908163 release-keys"
 
-BUILD_FINGERPRINT := google/sailfish/sailfish:10/QP1A.190711.020/5800535:user/release-keys
+BUILD_FINGERPRINT := google/sailfish/sailfish:10/QP1A.191005.007.A1/5908163:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT) \
